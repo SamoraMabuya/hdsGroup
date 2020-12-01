142429e27f0fac805faee9a9044b07f9d04d1c1a
Samora Mabuya <mrsamoramabuya@gmail.com>
2020-12-01 14:47:07 +0200

    update2

--- .history/resources/views/form.blade_20201201144621.php
+++ .history/resources/views/form.blade_20201201144621.php
@@ -7,7 +7,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
-    <link href="{{ asset('css/style.css')}}" rel="stylesheet"/>
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}"/>
     <script src="{{asset('js/javascript.js')}}" defer></script>
 
 </head>
