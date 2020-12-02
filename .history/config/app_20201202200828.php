0693fcf30417f69dadde618a52afcce1380d81f9
Samora Mabuya <mrsamoramabuya@gmail.com>
2020-12-02 20:09:06 +0200

    config

--- .history/config/app_20201202200828.php
+++ .history/config/app_20201202200828.php
@@ -63,7 +63,7 @@ return [
     'password' => env('MAIL_PASSWORD'),
     'driver' => env('MAIL_MAILER', 'smtp'),
     'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-    'port' => env('MAIL_PORT', 578),
+    'port' => env('MAIL_PORT', 2525),
     'from' => env('MAIL_ADDRESS', 'samora.sm.sam@gmail.com'),
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
     'username' => env('MAIL_USERNAME'),
