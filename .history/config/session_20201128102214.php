ed7c738ea2be02c97d4ab35f81ad95bdc340c59f
Samora Mabuya <mrsamoramabuya@gmail.com>
2020-12-06 15:43:56 +0200

    session domain

--- .history/config/session_20201128102214.php
+++ .history/config/session_20201128102214.php
@@ -155,7 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'https://hdspreview.herokuapp.com/'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
